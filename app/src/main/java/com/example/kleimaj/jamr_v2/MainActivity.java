ddf0ce18113ef9a9b4d48b24d50ea28bc1e8f802
Kleiman <@wwu.edu>
2018-10-19 12:23:26 -0700

    please work

---
+++ app/src/main/java/com/example/kleimaj/jamr_v2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.kleimaj.jamr_v2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_AppCompat_NoActionBar);
+        setContentView(R.layout.activity_main);
+    }
+}
