e94de571d61b169e4d05507d3931fb49b17a6b7f
Kleiman <@wwu.edu>
2018-10-20 17:52:19 -0700

    Band.java created

--- app/src/main/java/com/example/kleimaj/jamr_v2/Artist.java
+++ app/src/main/java/com/example/kleimaj/jamr_v2/Artist.java
@@ -1,5 +1,6 @@
 package com.example.kleimaj.jamr_v2;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
@@ -9,6 +10,55 @@ import java.util.ArrayList;
 public class Artist {
     private String name;
     private String bio;
+    private String gender;
+    private int age;
+    private ArrayList<String> identities; //e.g. Producer, Guitarist, Vocalist
+    private ArrayList<String> genres; //can switch to a String[] if need be
+
+    /*Constructors*/
+
+    public Artist(String name){ //for testing
+        this.name = name;
+        age = -1;
+        genres = new ArrayList<String>();
+        identities = new ArrayList<String>();
+    }
+
+    public Artist(String name, String gender) { //for testing
+        this.name = name;
+        this.gender = gender;
+        age = -1;
+        genres = new ArrayList<String>();
+        identities = new ArrayList<String>();
+    }
+
+    public Artist(String name, String gender, int age){ //for actual use
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        genres = new ArrayList<String>();
+        identities = new ArrayList<String>();
+    }
+
+    /*Setters*/
+
+    public void setBio(String bio){
+        this.bio = bio;
+    }
+
+    public void setGender(String gender){
+        this.gender = gender;
+    }
+
+    public void setGenres(ArrayList<String> genres){
+        this.genres = new ArrayList<String>(genres);
+    }
+
+    public void setIdentities(ArrayList<String> identities){
+        this.identities = identities;
+    }
+
+    /*Getters*/
 
     public String getName() {
         return name;
@@ -16,5 +66,19 @@ public class Artist {
 
     public String getBio() {
         return bio;
+    }
+    public String getGender(){
+        return gender;
+    }
+    public int getAge(){
+        return age;
+    }
+
+    public ArrayList<String> getIdentities() {
+        return identities;
+    }
+
+    public ArrayList<String> getGenres() {
+        return genres;
     }
 }
---
+++ app/src/main/java/com/example/kleimaj/jamr_v2/Band.java
@@ -0,0 +1,57 @@
+package com.example.kleimaj.jamr_v2;
+
+import android.media.Image;
+import android.widget.ImageView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by kleimaj on 10/20/18.
+ */
+
+public class Band {
+
+    private String name;
+    private String bio;
+    private ArrayList<ImageView> profilePictures;
+    private ArrayList<String> genres;
+
+    /* Constructor*/
+
+    public Band(String name){
+        this.name = name;
+    }
+
+    /*Setters*/
+
+    public void setBio(String bio){
+        this.bio = bio;
+    }
+
+    public void setGenres(ArrayList<String> genres){
+        genres = new ArrayList<String>(genres);
+    }
+
+    public void setProfilePictures(ArrayList<ImageView> pictures) {
+        profilePictures = new ArrayList<ImageView>(pictures);
+    }
+
+    /*Getters*/
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public ArrayList<String> getGenres() {
+        return genres;
+    }
+
+    public ArrayList<ImageView> getProfilePictures() {
+        return profilePictures;
+    }
+}
