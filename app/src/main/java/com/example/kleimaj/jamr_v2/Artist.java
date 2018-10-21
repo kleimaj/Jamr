b93956d531b208baa125cc85a6df6551cffab95b
Kleiman <@wwu.edu>
2018-10-20 17:13:29 -0700

    ProfileActivity.xml created, Artist.java model class created

---
+++ app/src/main/java/com/example/kleimaj/jamr_v2/Artist.java
@@ -0,0 +1,20 @@
+package com.example.kleimaj.jamr_v2;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kleimaj on 10/20/18.
+ */
+
+public class Artist {
+    private String name;
+    private String bio;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+}
