4d7ecf468597904c863101016be7426100d4ee64
Kleiman <@wwu.edu>
2018-10-20 19:05:09 -0700

    created MyInfo.xml, MyInfo.java

--- app/src/main/java/com/example/kleimaj/jamr_v2/MainActivity.java
+++ app/src/main/java/com/example/kleimaj/jamr_v2/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.kleimaj.jamr_v2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -12,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setTheme(R.style.Theme_AppCompat_NoActionBar);
         setContentView(R.layout.activity_main);
+    }
+
+    public void RegisterClick(View view){
+        Intent myIntent = new Intent(view.getContext(),Registration.class);
+        startActivity(myIntent);
     }
 }
