a10046149f403fa12e35131f6aaf533432670ea5
Owen <wabajack12@gmail.com>
2018-10-24 18:17:57 -0700

    Added icons to profile screen and if user is succussfully registered, profile screen is brought up

--- app/src/main/java/com/example/kleimaj/jamr_v2/Registration.java
+++ app/src/main/java/com/example/kleimaj/jamr_v2/Registration.java
@@ -1,5 +1,6 @@
 package com.example.kleimaj.jamr_v2;
 
+import android.content.Intent;
 import android.nfc.Tag;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -24,6 +25,7 @@ public class Registration extends AppCompatActivity {
     private EditText name,emailText,passwordText;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
+    private View passingView;
 
     private static final String TAG = "Registration";
 
@@ -82,6 +84,7 @@ public class Registration extends AppCompatActivity {
     }
 
     public void SignUpClick(View view){
+        passingView = view;
         if (email.isEmpty() || password.isEmpty()) {
             Toast.makeText(Registration.this, "Unfinished Sign-up Fields",Toast.LENGTH_SHORT).show();
         }
@@ -97,12 +100,13 @@ public class Registration extends AppCompatActivity {
 
                         Toast.makeText(Registration.this, "Register successful", Toast.LENGTH_SHORT).show();
                         FirebaseUser user = mAuth.getCurrentUser();
+                        Intent myIntent = new Intent(passingView.getContext(),Registration.class);
+                        startActivity(myIntent);
                     }
                 }
             });
         }
     }
-
     @Override
     protected void onStart() {
         super.onStart();
