4de3c73be38dc7a20423104fbc0f91ebbfd49807
Kleiman <@wwu.edu>
2018-11-01 21:00:58 -0700

    minor changes: SwipeScreen1 gradient, calls fragment1() in onCreate()

--- app/src/main/java/com/example/kleimaj/jamr_v2/MainActivity.java
+++ app/src/main/java/com/example/kleimaj/jamr_v2/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        switchToFragment1();
     }
 
     public void switchToFragment1() {
