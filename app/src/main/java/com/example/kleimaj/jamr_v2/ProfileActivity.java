48957a225adc1a18df9840b8d1c28df71f5e834c
kanghui liu <codingsupergirl@gmail.com>
2018-11-15 20:10:25 -0800

    at least something is working onchange

--- app/src/main/java/com/example/kleimaj/jamr_v2/ProfileActivity.java
+++ app/src/main/java/com/example/kleimaj/jamr_v2/ProfileActivity.java
@@ -3,6 +3,7 @@ package com.example.kleimaj.jamr_v2;
 import android.Manifest;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -16,9 +17,15 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -45,8 +52,28 @@ public class ProfileActivity extends Fragment implements View.OnClickListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //changeDisplay(MainActivity.currentUser.getName());
-        //setContentView(R.layout.activity_profile);
+        mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+        String current_uid = mCurrentUser.getUid();
+        mUserDatabase = FirebaseDatabase.getInstance().getReference().child
+                ("Users").child(current_uid);
+
+        mUserDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Toast.makeText(getActivity(), dataSnapshot.toString(), Toast
+                        .LENGTH_SHORT).show();
+                String name = dataSnapshot.child("name").getValue().toString();
+                String image = dataSnapshot.child("image").getValue()
+                        .toString();
+                String thumb_image = dataSnapshot.child("thumb_image")
+                        .getValue().toString();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
@@ -54,19 +81,11 @@ public class ProfileActivity extends Fragment implements View.OnClickListener {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.activity_profile, container, false);
-//        setDisplayName(view);
-//        setImage(view);
         Button  settingButton = view.findViewById(R.id.profile_setting_button);
         settingButton.setOnClickListener(this);
 
         Button  collectionButton = view.findViewById(R.id.profile_collection_button);
         collectionButton.setOnClickListener(this);
-//
-//        ImageButton infoRoundButton = view.findViewById(R.id.InfoRoundButton);
-//        infoRoundButton.setOnClickListener(this);
-//
-//        ImageButton profilePictureButton = view.findViewById(R.id.ProfilePictureButton);
-//        profilePictureButton.setOnClickListener(this);
 
         return view;
     }
@@ -83,34 +102,9 @@ public class ProfileActivity extends Fragment implements View.OnClickListener {
                 Intent settingIntent = new Intent(v.getContext(), MySettingsActivity.class);
                 this.startActivity(settingIntent);
                 break;
-//            case R.id.ProfilePictureButton:
-//                String[] permissionsList = new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
-//                            Manifest.permission.WRITE_EXTERNAL_STORAGE};
-//                requestPermissions(permissionsList, 1);
-//                Log.v("pok", "View Clicked");
-//                Intent i = new Intent(
-//                        Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-//                startActivityForResult(i, RESULT_LOAD_IMAGE);
         }
     }
 
-//    public void setImage(View v) {
-//        String image = MainActivity.currentUser.getImage();
-//        System.out.println("HERE ... IMAGE IS "+image);
-//        if (image!=null) {
-//            if (!image.isEmpty()) {
-//                //convert base64 to bitmap
-//                byte[] decodedBytes = Base64.decode(image, 0); //flag maybe Base64.DEFAULT
-//                ImageView imageView = (ImageView) v.findViewById(R.id.profile_image);
-//                imageView.setImageBitmap(BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes
-//                  .length));
-//            }
-//        }
-//    }
-//
-//    public void setDisplayName(View v) {
-//        TextView display = v.findViewById(R.id.ArtistName);
-//        display.setText(MainActivity.currentUser.getName());
-//    }
+
 
 }
