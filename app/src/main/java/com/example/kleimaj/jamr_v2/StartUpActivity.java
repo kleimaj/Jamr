145c90daad05aa96a8a42ab4f0a2d1d0584b5a1c
liuk5 <liuk5@wwu.edu>
2018-11-29 22:23:05 -0800

    Combine swipe with notification tutorial 23

--- app/src/main/java/com/example/kleimaj/jamr_v2/StartUpActivity.java
+++ app/src/main/java/com/example/kleimaj/jamr_v2/StartUpActivity.java
@@ -6,14 +6,20 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -27,6 +33,7 @@ public class StartUpActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
     static String UID = "";
+    private DatabaseReference mUserDatabase;
 
 
     @Override
@@ -36,6 +43,8 @@ public class StartUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start_up);
 
         mAuth = FirebaseAuth.getInstance(); //must initialize firebase auth
+        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+
         firebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) { //must initialize authstatelistener
@@ -55,7 +64,7 @@ public class StartUpActivity extends AppCompatActivity {
                     //Try Catch block to open/read files from directory and put into view
                     try {
                         FileInputStream stream = context.openFileInput(userId+"profileInfo" +
-                                ".txt");
+                          ".txt");
                         InputStreamReader streamReader = new InputStreamReader(stream);
                         reader = new BufferedReader(streamReader);
 
@@ -112,67 +121,85 @@ public class StartUpActivity extends AppCompatActivity {
             mAuth.signInWithEmailAndPassword(emailString,passwordString).addOnCompleteListener(StartUpActivity.this, new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()){ //firebase login failed
-                        Toast.makeText(StartUpActivity.this, "Sign-in Success",Toast.LENGTH_SHORT).show();
-                        //here we need to read from local file, store name and isBand in
-                        // MainActivity.currentUser
-                        System.out.println("Sign in Success, about to read file");
-                        Context context = getApplicationContext();
-                        BufferedReader reader = null;
-                       // StringBuilder text = new StringBuilder();
-                        String name = "";
-                        String isBand = "";
-                        String image = "";
-                        //Try Catch block to open/read files from directory and put into view
-                        try {
-                            System.out.println("USER ID IS : "+UID);
-                            FileInputStream stream = context.openFileInput(UID+"profileInfo" +
-                              ".txt");
-                            InputStreamReader streamReader = new InputStreamReader(stream);
-                            reader = new BufferedReader(streamReader);
+                    if (task.isSuccessful()){
 
-                            String line;
-                            int count = 0;
-                            System.out.println("ABOUT TO READ!!!");
-                            while((line = reader.readLine()) !=null){
-                                //text.append(line);
-                                //text.append('\n');
-                                if (count == 0) {
-                                    name = line;
-                                    System.out.println("PARSING THE FILE, HERE'S THE NAME!! : " +
-                                      ""+line);
-                                }
-                                else if (count == 1) {
-                                    isBand = line;
-                                }
-                                else if (count >= 2) {
-                                    image += line;
-                                }
-                                count++;
+                        // Get the token
+                        FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener( StartUpActivity.this,  new OnSuccessListener<InstanceIdResult>() {
+                            @Override
+                            public void onSuccess(InstanceIdResult instanceIdResult) {
+                                String current_user_id = mAuth.getCurrentUser().getUid();
+                                String deviceToken = instanceIdResult.getToken();
+
+                                mUserDatabase.child(current_user_id).child("device_token")
+                                  .setValue(deviceToken).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+
+                                        //here we need to read from local file, store name and isBand in
+                                        // MainActivity.currentUser
+                                        Context context = getApplicationContext();
+                                        BufferedReader reader = null;
+                                        // StringBuilder text = new StringBuilder();
+                                        String name = "";
+                                        String isBand = "";
+                                        String image = "";
+                                        //Try Catch block to open/read files from directory and put into view
+                                        try {
+                                            System.out.println("USER ID IS : "+UID);
+                                            FileInputStream stream = context.openFileInput(UID+"profileInfo" +
+                                              ".txt");
+                                            InputStreamReader streamReader = new InputStreamReader(stream);
+                                            reader = new BufferedReader(streamReader);
+
+                                            String line;
+                                            int count = 0;
+                                            System.out.println("ABOUT TO READ!!!");
+                                            while((line = reader.readLine()) !=null){
+                                                //text.append(line);
+                                                //text.append('\n');
+                                                if (count == 0) {
+                                                    name = line;
+                                                    System.out.println("PARSING THE FILE, HERE'S THE NAME!! : " +
+                                                      ""+line);
+                                                }
+                                                else if (count == 1) {
+                                                    isBand = line;
+                                                }
+                                                else if (count >= 2) {
+                                                    image += line;
+                                                }
+                                                count++;
+                                            }
+                                            reader.close();
+                                            stream.close();
+                                            streamReader.close();
+
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                        MainActivity.currentUser = new ArtistModel(name);
+                                        isBand = isBand.trim();
+                                        if (isBand.equals("true")) {
+                                            MainActivity.currentUser.setBand(true);
+                                        }
+                                        else {
+                                            MainActivity.currentUser.setBand(Boolean.parseBoolean(isBand));
+                                        }
+                                        MainActivity.currentUser.setImage(image);
+
+                                        Intent myIntent = new Intent(StartUpActivity.this, MainActivity.class);
+                                        startActivity(myIntent);
+                                        finish();
+
+
+
+                                    }
+                                });
+                                Log.e("newToken", deviceToken);
+
                             }
-                            reader.close();
-                            stream.close();
-                            streamReader.close();
+                        });
 
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        MainActivity.currentUser = new ArtistModel(name);
-                        isBand = isBand.trim();
-                        if (isBand.equals("true")) {
-                            System.out.println("HERE!!!");
-                            MainActivity.currentUser.setBand(true);
-                        }
-                        else {
-                            MainActivity.currentUser.setBand(Boolean.parseBoolean(isBand));
-                        }
-                        MainActivity.currentUser.setImage(image);
-                        System.out.println("THE FILE !!!!!!!! WWWWWWWWWWWWWW");
-                        System.out.println("MAIn BOOL IS : "+MainActivity.currentUser.isBand());
-
-                        Intent myIntent = new Intent(StartUpActivity.this, MainActivity.class);
-                        startActivity(myIntent);
-                        finish();
                     }
                     else {
                         Toast.makeText(StartUpActivity.this, "Sign-in Error",Toast.LENGTH_SHORT).show();
@@ -184,7 +211,7 @@ public class StartUpActivity extends AppCompatActivity {
     public void RegisterClick(View view){
         Intent myIntent = new Intent(view.getContext(),RegisterActivity.class);
         startActivity(myIntent);
-       // finish();
+        // finish();
     }
 
     @Override
